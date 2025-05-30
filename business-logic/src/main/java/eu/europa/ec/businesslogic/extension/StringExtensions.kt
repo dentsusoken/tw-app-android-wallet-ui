2f725e7e7dbe344f22533d7ad46f93b6d4f5d59c
“ryohei-ebisawa” <xebisawa.ryohei@contract.isid.co.jp>
2025-05-30 15:11:02 +0900

    re-upload pt1

---
+++ business-logic/src/main/java/eu/europa/ec/businesslogic/extension/StringExtensions.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be approved by the European
+ * Commission - subsequent versions of the EUPL (the "Licence"); You may not use this work
+ * except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the Licence for the specific language
+ * governing permissions and limitations under the Licence.
+ */
+
+package eu.europa.ec.businesslogic.extension
+
+import android.net.Uri
+import android.util.Base64
+import com.google.gson.Gson
+import java.net.URLEncoder
+
+fun String.encodeToBase64(): String = Base64.encodeToString(
+    this.toByteArray(Charsets.UTF_8),
+    Base64.NO_WRAP or Base64.NO_PADDING or Base64.URL_SAFE
+)
+
+fun String.decodeFromBase64(): String = Base64.decode(
+    this, Base64.NO_WRAP or Base64.NO_PADDING or Base64.URL_SAFE
+).toString(Charsets.UTF_8)
+
+fun String.urlEncode(): String = URLEncoder.encode(this, "UTF-8")
+
+fun String.toUri(): Uri = try {
+    Uri.parse(this)
+} catch (e: Exception) {
+    Uri.EMPTY
+}
+
+fun String.validateAndFormatUrl(): String {
+    if (isEmpty() || contains("http") || contains("https")) {
+        return this
+    }
+    return "https://${this.lowercase()}"
+}
+
+inline fun <reified T> String.parseFromJson(): T? {
+    return try {
+        Gson().fromJson(this, T::class.java)
+    } catch (_: Exception) {
+        null
+    }
+}
+
+/**
+ * Converts Pem base64 String to Byte Array
+ *
+ * @receiver String object
+ * @return Byte Array object
+ */
+fun String.decodeFromPemBase64String(): ByteArray? {
+    return Base64.decode(this.replace("\n", ""), Base64.NO_WRAP)
+}
+
+/**
+ * String split into substrings. The maximum length of the substring
+ * is determined by a parameter @param lineLength
+ *
+ * @receiver String object
+ * @param lineLength - maximum number of characters per line
+ * @return String object
+ */
+fun String.splitToLines(lineLength: Int): String {
+    return if (lineLength <= 0 || this.length <= lineLength) this
+    else {
+        var result = ""
+        var index = 0
+        while (index < length) {
+            val line = substring(index, Math.min(index + lineLength, length))
+            result += (if (result.isEmpty()) line else "\n$line")
+            index += lineLength
+        }
+        result
+    }
+}
+
+fun String.firstPart(separator: String): String = this.split(separator).firstOrNull() ?: this
---
+++ common-feature/src/main/java/eu/europa/ec/commonfeature/config/SuccessUIConfig.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be approved by the European
+ * Commission - subsequent versions of the EUPL (the "Licence"); You may not use this work
+ * except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the Licence for the specific language
+ * governing permissions and limitations under the Licence.
+ */
+
+package eu.europa.ec.commonfeature.config
+
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import eu.europa.ec.resourceslogic.theme.values.ThemeColors
+import eu.europa.ec.uilogic.config.ConfigNavigation
+import eu.europa.ec.uilogic.config.NavigationType
+import eu.europa.ec.uilogic.serializer.UiSerializable
+import eu.europa.ec.uilogic.serializer.UiSerializableParser
+import eu.europa.ec.uilogic.serializer.adapter.SerializableTypeAdapter
+
+data class SuccessUIConfig(
+    val headerConfig: HeaderConfig?,
+    val content: String,
+    val imageConfig: ImageConfig,
+    val buttonConfig: List<ButtonConfig>,
+    val onBackScreenToNavigate: ConfigNavigation
+) : UiSerializable {
+
+    data class ImageConfig(
+        val type: Type,
+        @DrawableRes val drawableRes: Int? = null,
+        val tint: Color = ThemeColors.success,
+        val contentDescription: String? = null
+    ) {
+        enum class Type {
+            DRAWABLE, DEFAULT
+        }
+    }
+
+    data class ButtonConfig(
+        val text: String,
+        val style: Style,
+        val navigation: ConfigNavigation,
+    ) {
+        enum class Style {
+            PRIMARY, OUTLINE
+        }
+    }
+
+    data class HeaderConfig(
+        val title: String,
+        val color: Color = ThemeColors.success
+    )
+
+    companion object Parser : UiSerializableParser {
+        override val serializedKeyName = "successConfig"
+        override fun provideParser(): Gson {
+            return GsonBuilder().registerTypeAdapter(
+                NavigationType::class.java,
+                SerializableTypeAdapter<NavigationType>()
+            ).create()
+        }
+    }
+}
